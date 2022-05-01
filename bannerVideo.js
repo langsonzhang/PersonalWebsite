c7c2c9c26b4aa888a04e6e3ffb492219e708024b
Langson <langson.zhang@mail.utoronto.ca>
2022-05-01 01:12:23 -0400

    added name:

---
+++ bannerVideo.js
@@ -0,0 +1,17 @@
+var hueVal = 0
+var blurVal = "0px"
+var contrastVal = "82%"
+var invertVal = "8%"
+var brightnessVal = "120%"
+
+var video = document.getElementById("video")
+var videoCont = document.getElementById("cont1")
+videoCont.addEventListener('mousemove', updateFilters)
+updateFilters()
+
+function updateFilters(event) {
+    var filterVal = `hue-rotate(${Math.floor(hueVal / 15) % 360}deg)`
+    filterVal += `brightness(${brightnessVal}) contrast(${contrastVal}) blur(${blurVal}) saturate(110%) invert(${invertVal})`
+    video.style.filter = filterVal
+    hueVal--
+}
