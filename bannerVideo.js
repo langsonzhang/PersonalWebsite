43c14e61db936af495d650a8b321db415427ce71
Langson <langson.zhang@mail.utoronto.ca>
2022-05-09 20:32:36 -0700

    updated

--- bannerVideo.js
+++ bannerVideo.js
@@ -1,8 +1,8 @@
 var hueVal = 0
-var blurVal = "0px"
-var contrastVal = "100%"
-var invertVal = "15%"
-var brightnessVal = "110%"
+var blurVal = 0
+var contrastVal = 100
+var invertVal = 15
+var brightnessVal = 110
 
 var video = document.getElementById("video")
 var videoCont = document.getElementById("cont1")
@@ -11,7 +11,7 @@ updateFilters()
 
 function updateFilters(event) {
     var filterVal = `hue-rotate(${Math.floor(hueVal / 15) % 360}deg)`
-    filterVal += `brightness(${brightnessVal}) contrast(${contrastVal}) blur(${blurVal}) saturate(110%) invert(${invertVal})`
+    filterVal += `brightness(${brightnessVal}%) contrast(${contrastVal}%) blur(${blurVal}px) saturate(110%) invert(${invertVal}%)`
     video.style.filter = filterVal
     hueVal--
 }
